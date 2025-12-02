bd1a7c79b8022bfd65117c9b4b7933b6da53e78c
kannokeita0430 <kuriarimu330127@gmail.com>
2025-12-02 11:01:22 +0900

    サイズと盤の配置

--- 2025_kaihatu_kanno/2025_kaihatu_kanno/Board.cs
+++ 2025_kaihatu_kanno/2025_kaihatu_kanno/Board.cs
@@ -8,9 +8,27 @@ namespace _2025_kaihatu_kanno
 {
     public class Board
     {
-        //オセロの盤面８×８をつくる
+        // オセロの盤面８×８をつくる
         public const int SIZE = 8;
         public char[,] Cells = new char[SIZE, SIZE];
-        public char CurrentPlayer = '●';    
+        public char CurrentPlayer = '●';
+
+        public Board()
+        {
+            InitializeBoard();
+        }
+
+        // 盤の初期配置
+        public void InitializeBoard()
+        {
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++)
+                    Cells[i, j] = '・'; // 空白
+            // 真ん中の初期位置に黒と白を配置
+            Cells[3, 3] = '○';
+            Cells[3, 4] = '●';
+            Cells[4, 3] = '●';
+            Cells[4, 4] = '○';
+        }
     }
 }
--- 2025_kaihatu_kanno/2025_kaihatu_kanno/Form1.cs
+++ 2025_kaihatu_kanno/2025_kaihatu_kanno/Form1.cs
@@ -12,6 +12,20 @@ namespace _2025_kaihatu_kanno
 {
     public partial class Form1 : Form
     {
-      
+        // Boardクラス使う
+        Button[,] buttons = new Button[Board.SIZE, Board.SIZE];
+        Board board = new Board(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // サイズ
+            this.ClientSize = new Size(420, 420);
+            
+        }
     }
 }
