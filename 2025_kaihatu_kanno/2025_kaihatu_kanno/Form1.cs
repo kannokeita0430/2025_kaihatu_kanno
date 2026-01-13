76890026d31a06bc09ceee63df4b2e780c88210f
kiraquin0623 <kiraquin0623@gmail.com>
2026-01-13 10:13:00 +0900

    仕方ないので上げます

--- 2025_kaihatu_kanno/2025_kaihatu_kanno/Form1.cs
+++ 2025_kaihatu_kanno/2025_kaihatu_kanno/Form1.cs
@@ -24,7 +24,7 @@ namespace _2025_kaihatu_kanno
         private void Form1_Load(object sender, EventArgs e)
         {
             // 画面サイズ
-            this.ClientSize = new Size(800, 750);
+            this.ClientSize = new Size(700, 700);
             
             // そもそものボード
             CreateBoardUI();
@@ -37,7 +37,7 @@ namespace _2025_kaihatu_kanno
         private void CreateBoardUI()
         {
             // ボタンのサイズ
-            int size = 80;
+            int size = 65;
 
             for (int i = 0; i < Board.SIZE; i++)
             {
